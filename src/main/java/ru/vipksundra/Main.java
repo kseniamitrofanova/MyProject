a2bd2f0f2ea7284f54d2b6b99e7b12e33e7fa76b
Kseniya Mitrofanova <KMitrofanova@amdocs.com>
2023-10-26 15:55:02 +0300

    first commit

--- src/main/java/ru/vipksundra/Main.java
+++ src/main/java/ru/vipksundra/Main.java
@@ -2,6 +2,6 @@ package ru.vipksundra;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qu guru!");
+        System.out.println("Hello qu Quru!!");
     }
 }
