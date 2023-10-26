fb3eea836ac3a614e2cd33ce5559c9e6665e95ac
kseniamitrofanova <73057140+kseniamitrofanova@users.noreply.github.com>
2023-10-26 16:52:54 +0300

    Update Main.java

--- src/main/java/ru/vipksundra/Main.java
+++ src/main/java/ru/vipksundra/Main.java
@@ -1,7 +1,10 @@
 package ru.vipksundra;
 
 public class Main {
-    private static String msg = "Hello Hello!";
+
+
+    
+    private static String msg = "Hello Hello!!!!";
     public static void main(String... args) {
         System.out.println(msg);
     }
