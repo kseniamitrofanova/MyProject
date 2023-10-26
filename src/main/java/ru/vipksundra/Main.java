7de3f5f1699bd9086a20c902e564fee4cca902d1
Kseniya Mitrofanova <KMitrofanova@amdocs.com>
2023-10-26 16:06:49 +0300

    Hello

--- src/main/java/ru/vipksundra/Main.java
+++ src/main/java/ru/vipksundra/Main.java
@@ -2,6 +2,6 @@ package ru.vipksundra;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qu Quru!!");
+        System.out.println("Hello Hello");
     }
 }
