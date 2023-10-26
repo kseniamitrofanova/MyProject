b42c84ef94d59f5d2c212123b99b8a5be4852474
kseniamitrofanova <73057140+kseniamitrofanova@users.noreply.github.com>
2023-10-26 16:19:16 +0300

    Update Main.java

--- src/main/java/ru/vipksundra/Main.java
+++ src/main/java/ru/vipksundra/Main.java
@@ -2,6 +2,6 @@ package ru.vipksundra;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Hello");
+        System.out.println("Hello Hello!!!");
     }
 }
